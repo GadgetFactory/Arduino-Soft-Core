d0769d95e150598a5782349cf824da5ea87b6517
jackgassett <jack.gassett@gadgetfactory.net>
2011-03-20 23:26:32 -0600

    Moved scripts to better place.

--- copy-to-custom.cmd
+++
@@ -1,6 +0,0 @@
-REM Define the base directory for your Arduino IDE installation.
-set arduino-base="C:\arduino"
-
-copy scripts\XilinxISE\Papilio_AVR8.bit %arduino-base%\hardware\tools\butterfly_platform\bitstreams\custom.bit
-copy scripts\XilinxISE\Papilio_AVR8_bd.bmm %arduino-base%\hardware\tools\butterfly_platform\bitstreams\custom_bd.bmm
-pause
